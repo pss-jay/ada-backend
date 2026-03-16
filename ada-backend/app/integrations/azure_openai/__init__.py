c21a857412cc20404bb511fd3e434101af545fe0
pss-jay <jaivardhang@puresoftsolutions.in>
2026-03-16 09:58:27 +0530

    Delete ada-backend directory

--- ada-backend/app/integrations/azure_openai/__init__.py
+++
@@ -1,9 +0,0 @@
-from .schema_models import (
-    CommonProtocolModel,
-    RatProtocolModel,
-    DogProtocolModel,
-    SwineProtocolModel,
-    SPECIES_MODEL_MAP,
-)
-from .primary_prompt import PrimaryPromptGenerator
-from .prompt_helper import AzureOpenAIClient, PromptOrchestrator, _parse_json_text
